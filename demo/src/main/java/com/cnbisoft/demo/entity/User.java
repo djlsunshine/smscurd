7aa5eb4632ffa373f6fbebbf808f5688ed2563da
djl <djl@cnbisoft.com>
2020-12-20 23:03:28 +0800

    update user

--- demo/src/main/java/com/cnbisoft/demo/entity/User.java
+++ demo/src/main/java/com/cnbisoft/demo/entity/User.java
@@ -19,7 +19,7 @@ import java.io.Serializable;
 //@AllArgsConstructor
 public class User implements Serializable {
 
-//    public static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
     private Integer id;
     private String userName;
     private String address;
